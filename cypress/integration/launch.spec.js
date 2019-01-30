4b2d3e85e61561eb7d145d23a74fd863f2687115
Ysrael Torres <ysraeltorres04@gmail.com>
2019-01-30 11:35:04 -0500

    yml update

--- cypress/integration/launch.spec.js
+++ cypress/integration/launch.spec.js
@@ -3,16 +3,11 @@ describe('login', function () {
     const pass =  Cypress.env('password');
     beforeEach(() => {
 
-        cy.visit('https://www.r8.beer/')
+        cy.visit('http://0.0.0.0:3000/')
 
-        cy.get('#headerLogIn', { timeout: 10000 }).click()
-        // cy.get('#headerLogIn').click()
+
     })
     it('Should log in', function () {
-        cy.get('#auth0-lock-container-1 > div > div.auth0-lock-center > form > div > div > div:nth-child(3) > span > div > div > div > div > div > div > div > div > div:nth-child(4) > div.auth0-lock-input-block.auth0-lock-input-username > div > input')
-            .type(user)
-        cy.get('#auth0-lock-container-1 > div > div.auth0-lock-center > form > div > div > div:nth-child(3) > span > div > div > div > div > div > div > div > div > div:nth-child(4) > div.auth0-lock-input-block.auth0-lock-input-show-password > div.auth0-lock-input-block.auth0-lock-input-password > div > input')
-            .type(pass)
-        cy.get('#auth0-lock-container-1 > div > div.auth0-lock-center > form > div > div > button > span').click()
+        cy.contains('body', 'Hello Node.js IZ sample!', { timeout: 10000 })
     })
 })
